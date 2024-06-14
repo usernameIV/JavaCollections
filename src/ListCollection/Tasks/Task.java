2b6d94c6f9c8db2a393eb7dfb9303468197c56b1
usernameIV <researches.dev.pro@gmail.com>
2024-06-13 21:22:38 -0300

    Task List Files

---
+++ src/ListCollection/Tasks/Task.java
@@ -0,0 +1,19 @@
+package ListCollection.Tasks;
+
+public class Task {
+
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "ListCollection.Tasks:"+" descriptions = " + description;
+    }
+}
---
+++ src/ListCollection/Tasks/TaskList.java
@@ -0,0 +1,36 @@
+package ListCollection.Tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+    private List<Task> tasksList;
+
+    public TaskList () {
+        this.tasksList = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        tasksList.add(new Task(description));
+    }
+    public void removeTask(String description) {
+        List<Task> toRemoveTasks = new ArrayList<>();
+        for (Task task : tasksList) {
+            if(task.getDescription().equalsIgnoreCase(description)) {
+             toRemoveTasks.add(task);
+            }
+        }
+        tasksList.removeAll(toRemoveTasks);
+    }
+
+    public int getAllTasks() {
+        return tasksList.size();
+    }
+
+    public void getTasksDescription() {
+        System.out.println(tasksList);
+    }
+
+
+}
