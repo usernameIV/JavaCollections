093c6cc52992888e7d755d0e9559d3c36fc05486
usernameIV <researches.dev.pro@gmail.com>
2024-06-18 19:15:46 -0300

    ComparatorPerGrade class

--- src/SetColletion/SortSet/StudentsList/ComparatorPerGrade.java
+++ src/SetColletion/SortSet/StudentsList/ComparatorPerGrade.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class ComparatorPerGrade implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
-        return Double.compare(s1.getGrade(), s2.getGrade());
+        return Double.compare(s1.getGrade(), s2.getGrade()  );
     }
 }
